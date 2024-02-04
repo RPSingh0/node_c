9e03f3cbed02a1da221a122b7796085641a87294
RPSingh0 <rpalsingh715@gmail.com>
2024-02-04 22:47:05 +0530

    completed refactoring ✅

--- natours/app.js
+++ natours/app.js
@@ -19,8 +19,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4. SERVER
-const port = 3000;
-app.listen(port, '127.0.0.1', () => {
-    console.log(`App running on port: ${port}...`);
-});
+module.exports = app;
