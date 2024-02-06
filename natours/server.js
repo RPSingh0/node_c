f7347dfdad945ba0789d6c39043ace47fd019488
RPSingh0 <rpalsingh715@gmail.com>
2024-02-06 12:25:51 +0530

    before refactoring for MVC architecture 🍉

--- natours/server.js
+++ natours/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // we have to set it up before running app, so that all variable are available there...
@@ -6,8 +7,51 @@ dotenv.config({
 });
 
 const app = require('./app');
+const {SchemaType} = require("mongoose");
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(connection => {
+    // console.log(connection.connections);
+    console.log('DB connection successful');
+});
 // console.log(app.get('env'));
 // console.log(process.env);
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The park camper',
+    price: 997
+});
+
+testTour
+    .save()
+    .then(document => {
+        console.log(document);
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
