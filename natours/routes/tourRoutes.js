2f5be973aa55c4f45a7b6b2e5076fa1a1636fb4b
RPSingh0 <rpalsingh715@gmail.com>
2024-02-08 10:54:49 +0530

    before modelling for tours 🫠

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const tourController = require('./../controllers/toursController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-// adding a middleware that will run only when we have the 'id' parameter.
-router.param('id', tourController.checkID);
-
 router.route('/')
     .get(tourController.getAllTours)
-    .post(tourController.checkBody, tourController.createTour);
+    .post(tourController.createTour);
 
 router.route('/:id')
     .get(tourController.getTour)
--- natours/server.js
+++ natours/server.js
@@ -7,51 +7,16 @@ dotenv.config({
 });
 
 const app = require('./app');
-const {SchemaType} = require("mongoose");
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
 }).then(connection => {
-    // console.log(connection.connections);
-    console.log('DB connection successful');
+    console.log('Connected to MongoDB Atlas!...');
 });
-// console.log(app.get('env'));
-// console.log(process.env);
-
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-    name: 'The park camper',
-    price: 997
-});
-
-testTour
-    .save()
-    .then(document => {
-        console.log(document);
-    })
-    .catch(error => {
-        console.log('Error: ', error);
-    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
