dda8b66b9bf8671dbe0016cd8a26cb91594f6798
RPSingh0 <rpalsingh715@gmail.com>
2024-02-14 19:13:43 +0530

    before implementing common error handler ❌

--- natours/app.js
+++ natours/app.js
@@ -25,4 +25,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// if not handled by any of the router
+app.all('*', (req, res, next) => {
+    res.status(404)
+        .json({
+            status: 'fail',
+            message: `Can't find ${req.originalUrl} on this server!`
+        })
+});
+
 module.exports = app;
