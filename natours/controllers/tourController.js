63968972b47b89370e0cc865663cc063d502a939
RPSingh0 <rpalsingh715@gmail.com>
2024-02-14 13:55:00 +0530

    complete 8 sections 💃🏼🎉

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -71,7 +71,7 @@ exports.createTour = async (req, res) => {
         res.status(400)
             .json({
                 status: 'fail',
-                message: 'Invalid data sent!'
+                message: error
             });
     }
 }
--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+        minlength: [10, 'A tour name must have more or equal then 10 characters']
+        // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -19,11 +23,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour should have a difficulty']
+        required: [true, 'A tour should have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
         type: Number,
@@ -33,7 +43,18 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function (val) {
+                // this only points to current doc on NEW document creation
+                return val < this.price; // 100 < 200
+            },
+            message: 'Discount price ({VALUE}) should be below regular price'
+        }
+
+
+    },
     summary: {
         type: String,
         trim: true,
@@ -97,8 +118,8 @@ tourSchema.pre(/^find/, function (next) {
 })
 
 tourSchema.post(/^find/, function (docs, next) {
-    console.log(`Query took: ${Date.now() - this.start} milliseconds !`);
-    console.log(docs);
+    // console.log(`Query took: ${Date.now() - this.start} milliseconds !`);
+    // console.log(docs);
     next();
 });
 
@@ -125,7 +146,7 @@ tourSchema.pre('aggregate', function (next) {
 ]
 
      */
-    console.log(this.pipeline());
+    // console.log(this.pipeline());
     next();
 });
 
