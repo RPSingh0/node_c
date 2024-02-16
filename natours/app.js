0394adc1057eaca5b3fedecb70cd1a03f9986507
RPSingh0 <rpalsingh715@gmail.com>
2024-02-16 17:58:14 +0530

    completed section 9 🥳

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -27,11 +29,18 @@ app.use('/api/v1/users', userRouter);
 
 // if not handled by any of the router
 app.all('*', (req, res, next) => {
-    res.status(404)
-        .json({
-            status: 'fail',
-            message: `Can't find ${req.originalUrl} on this server!`
-        })
+    // res.status(404)
+    //     .json({
+    //         status: 'fail',
+    //         message: `Can't find ${req.originalUrl} on this server!`
+    //     })
+    // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    // err.status = 'fail';
+    // err.statusCode = 404;
+
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- natours/server.js
+++ natours/server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+    console.log('Unhandled Exception! ❌  Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+})
+
+
 // we have to set it up before running app, so that all variable are available there...
 dotenv.config({
     path: './config.env'
@@ -19,6 +26,24 @@ mongoose.connect(DB, {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, '127.0.0.1', () => {
+const server = app.listen(port, '127.0.0.1', () => {
     console.log(`App running on port: ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+    console.log('Unhandled Rejection! ❌  Shutting down...');
+    console.log(err);
+    server.close(() => {
+        process.exit(1);
+    })
+});
+
+// process.on('uncaughtException', err => {
+//     console.log('Unhandled Exception! ❌  Shutting down...');
+//     console.log(err.name, err.message);
+//     server.close(() => {
+//         process.exit(1);
+//     })
+// })
+
+// console.log(x);
