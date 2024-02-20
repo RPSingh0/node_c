278f039d90ce8a21283b851c8105c6ad56e262ff
RPSingh0 <rpalsingh715@gmail.com>
2024-02-20 17:50:36 +0530

    completed section 11 🕚

--- natours/app.js
+++ natours/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xxs = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,16 +13,42 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// set security HTTP headers
+app.use(helmet());
+
 // 1. MIDDLEWARES
+// development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// Rate limiter from sam api
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+})
+
+app.use('/api', limiter);
+
+// body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+// data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// prevent parameter pollution
+app.use(hpp({
+    whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}));
 
 // MIDDLEWARE for serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -14,6 +14,20 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id);
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        // secure: true, // allow transfer of cookie only through https
+        httpOnly: true // prevent browser from changing the cookie in any way
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+        cookieOptions.secure = true;
+    }
+
+    res.cookie('jwt', token, cookieOptions);
+
+    // remove the password from view
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
