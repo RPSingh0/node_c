1c976d35d2ebd58d368adaa7449764b953759d80
RPSingh0 <rpalsingh715@gmail.com>
2024-02-23 12:06:02 +0530

    completeactory methods 🏭🧑🏼‍🏭

--- natours/routes/reviewRoutes.js
+++ natours/routes/reviewRoutes.js
@@ -8,6 +8,11 @@ const router = express.Router({
 
 router.route('/')
     .get(reviewController.getAllReviews)
-    .post(authController.protect, authController.restrictTo('user'), reviewController.createReview);
+    .post(authController.protect, authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview);
+
+router.route('/:id')
+    .get(reviewController.getReview)
+    .patch(reviewController.updateReview)
+    .delete(reviewController.deleteReview);
 
 module.exports = router;
