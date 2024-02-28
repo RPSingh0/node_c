db2e3892986d6b7af3065b13c36ac0ae237f008f
RPSingh0 <rpalsingh715@gmail.com>
2024-02-28 12:14:38 +0530

    done with pug basics 🐶

--- natours/app.js
+++ natours/app.js
@@ -11,13 +11,21 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const path = require("path");
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // set security HTTP headers
 app.use(helmet());
 
 // 1. MIDDLEWARES
+// MIDDLEWARE for serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
@@ -46,13 +54,30 @@ app.use(hpp({
     whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
 }));
 
-// MIDDLEWARE for serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
+});
+
+// PUG routes
+app.get('/', (req, res) => {
+    res.status(200).render('base', {
+        tour: 'The forest hiker',
+        user: 'Jonas'
+    });
+});
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All tours'
+    })
+});
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour'
+    })
 });
 
 // MOUNTING THE ROUTERS
